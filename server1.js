e65e63350d2a701d38932d87c8e4efd1ef47d16a
abdubash04 <165327233+abdubash04@users.noreply.github.com>
2024-05-31 16:57:36 +0100

    final touches

--- server1.js
+++ server1.js
@@ -16,83 +16,115 @@ const MongoClient = require('mongodb').MongoClient;
 
 let db;
 
-MongoClient.connect('mongodb+srv://abdubash04:Family01234@cluster0.datjqek.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0', (err, Client)=>{
+MongoClient.connect('mongodb+srv://abdubash04:Family01234@cluster0.datjqek.mongodb.net/', (err, Client)=>{
     db = Client.db('webstore');
-})
-
-app.get('/', (req, res, next) => {
-    res.send('Select a collection, e.g., /collection/messages')
-})
-
-app.post("/search/collection/:collectionName/", (req, res, next) => {
-    var search = req.body.search;
-    var sort = req.body.sort || "title";
-    var order = req.body.order == "desc" ? -1 : 1;
-  
-    if (search) {
-      search = {
-        $or: [
-          { title: { $regex: search, $options: "i" } },
-          { subject: { $regex: search, $options: "i" } },
-          { location: { $regex: search, $options: "i" } },
-        ],
-      };
-    } else {
-      search = {};
-    }
-  
-    db.collection(req.params.collectionName)
-      .find(search)
-      .sort({ [sort]: order })
-      .toArray((e, results) => {
-        if (e) return next(e);
-        res.send(results);
-      });
+})          
+app.use((req, res, next) => {
+    var log = `${req.ip} -- ${req.method} ${req.path} ${res.statusCode}"`;
+    console.log(log, req.body);
+    next();
   });
   
-
-app.post('/collection/:collectionName', (req, res, next) => {
-    req.collection.insert(req.body, (e, results) => {
-        if (e) return next(e)
-            res.send(results.ops);
-        
-    })
-})
-
-app.param('collectionName', (req, res, next, collectionName) => {
-    req.collection = db.collection(collectionName)
-    console.log(req);
-        return next()
-    })
-
-app.get('/collection/:collectionName', (req, res, next) =>{
-    req.collection.find({}).toArray((e, results) => {
-        if (e) return next(e)
-            res.send(results)
-        
-    })
-})
-
-app.put('/collection/:collectionName/:id', (req, res, next)=>{
-    req.collection.update(
-        {_id: new objectID(req.params.id)},
-        {$set: req.body},
-        {safe: true, multi: false},
-        (e, result) => {
-            if (e) return next (e)
-                res.send((result.result.n === 1) ? {msg: 'success'} : {msg : 'error'})
-        }
-    )
-})
-
-app.delete('collection/:collectionName/:id', (req, res, next) => {
-    req.collection.deleteOne(
-        {_id: objectID(req.params.id)},(e, result) => {
-            if (e) return next (e)
-                res.send((result.result.n === 1)?
-            {msg: 'success'} : {msg: 'error'})
-        } )
-})
-// app.listen(3000, ()=>{
-//     console.log('express.js server running at localhost:3000');
-// })
+  app.get("/", (req, res) => {
+    // db.collection('lessons').updateMany({}, { $set: { avaliability: 5 } });
+    res.send("Select a collection, e.g., /collection/products");
+  });
+  
+  // retrieve all the object from an collection
+  app.get("/collection/:collectionName", (req, res) => {
+    try {
+      db.collection(req.params.collectionName)
+        .find({})
+        .toArray()
+        .then((results) => {
+          res.send(results);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  // Search
+  app.post("/search/collection/products/", (req, res) => {
+    try {
+      var search = req.body.search;
+      var sort = req.body.sort || "title";
+      var order = req.body.order == "desc" ? -1 : 1;
+  
+      if (search) {
+        search = {
+          $or: [
+            { title: { $regex: search, $options: "i" } },
+            { subject: { $regex: search, $options: "i" } },
+            { location: { $regex: search, $options: "i" } },
+          ],
+        };
+      } else {
+        search = {};
+      }
+  
+      db.collection("products")
+        .find(search)
+        .sort({ [sort]: order })
+        .toArray()
+        .then((results) => {
+          res.send(results);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  //to insert a document to the collection
+  app.post("/collection/:collectionName", (req, res) => {
+    try {
+      db.collection(req.params.collectionName)
+        .insertOne(req.body)
+        .then((results) => {
+          res.send(results);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  app.get("/collection/:collectionName/:id", (req, res) => {
+    try {
+      db.collection(req.params.collectionName)
+        .findOne({ _id: new ObjectId(req.params.id) })
+        .then((results) => {
+          res.send(results);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  //to update a document by ID
+  app.put("/collection/:collectionName/:id", (req, res) => {
+    try {
+      db.collection(req.params.collectionName)
+        .updateOne({ _id: new ObjectId(req.params.id) }, { $set: req.body })
+        .then((results) => {
+          res.send(results);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  app.delete("/collection/:collectionName/:id", (req, res) => {
+    try {
+      db.collection(req.params.collectionName)
+        .deleteOne({ _id: ObjectId(req.params.id) })
+        .then((results) => {
+          res.send(results);
+        });
+    } catch (error) {
+      console.log(error);
+    }
+  });
+  
+  app.listen(3000, () => {
+    console.log("Express.js server running at PORT 3000");
+  });
